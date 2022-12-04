bc66b22e6e3653905bf1b83ea6c5bc1ce775d766
zakarish <90837069+zakarish@users.noreply.github.com>
2022-12-04 17:40:40 +0100

    Create konekt.php

---
+++ konekt.php
@@ -0,0 +1,7 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "inlamning4";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+>
