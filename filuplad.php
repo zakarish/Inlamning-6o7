f51cb17b5b822bef34a63e17cb56895b2056d96b
zakarish <90837069+zakarish@users.noreply.github.com>
2022-12-04 17:42:05 +0100

    Create filuplad.php

---
+++ filuplad.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="uploadOk.php" method="POST" enctype="multipart/form-data">
+        <label for="file">välj fil</label>
+        <input type="file" name="fileToUpload">
+        <button type="submit" name="submit" value="submit">Upload file</button>
+    </form>
+</body>
+</html>
