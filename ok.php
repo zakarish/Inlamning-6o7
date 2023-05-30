9c03aef7d0c482a80ca678d73d405cebd0b857dd
zakarish <90837069+zakarish@users.noreply.github.com>
2023-05-30 17:08:54 +0200

    Update ok.php

--- ok.php
+++ ok.php
@@ -1,24 +1,23 @@
 <?php
 session_start();
-include "connection.php";
+include "konekt.php";
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
 $filename = $_FILES['fileToUpload']['name'];
 
 if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-  echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.";
-  if ($_SESSION["username"] == "holros") {
-    $sql = "INSERT INTO uploads (filename, user, uploadtime, snuskig)
+    echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.";
+    if ($_SESSION["username"] == "holros") {
+        $sql = "INSERT INTO uploads (filename, user, uploadtime, snuskig)
         VALUES ('$filename', '" . $_SESSION["username"] . "', NOW(), TRUE)";
-    $conn->query($sql);
-  } else {
-    $test =  $_SESSION["username"];
-    $sql = "INSERT INTO uploads (filename, user, uploadtime) VALUES ('$filename', '$test', NOW())";
-    $result = $conn->query($sql);
-  }
+        $conn->query($sql);
+    } else {
+        $test =  $_SESSION["username"];
+        $sql = "INSERT INTO uploads (filename, user, uploadtime) VALUES ('$filename', '$test', NOW())";
+        $result = $conn->query($sql);
+    }
 } else {
-  echo "Sorry, there was an error uploading your file.";
+    echo "Sorry, there was an error uploading your file.";
 }
